90b3d941416aca4e7f5b10946e03d1c1159e4f08
opdt <IT-Systemhaus.SEP5-Online-Team-OPDT@arbeitsagentur.de>
2023-02-07 17:53:11 +0100

    refactoring: remove UserProviderFactory to make sure the cassandra provider is loaded

--- core/src/main/java/de/arbeitsagentur/opdt/keycloak/cassandra/CassandraMapDatastoreProviderFactory.java
+++ core/src/main/java/de/arbeitsagentur/opdt/keycloak/cassandra/CassandraMapDatastoreProviderFactory.java
@@ -20,12 +20,15 @@ import lombok.extern.jbosslog.JBossLog;
 import org.keycloak.Config;
 import org.keycloak.models.*;
 import org.keycloak.provider.EnvironmentDependentProviderFactory;
+import org.keycloak.provider.InvalidationHandler;
 import org.keycloak.storage.DatastoreProvider;
 import org.keycloak.storage.DatastoreProviderFactory;
 
+import static org.keycloak.models.map.common.AbstractMapProviderFactory.MapProviderObjectType.*;
+
 @JBossLog
 @AutoService(DatastoreProviderFactory.class)
-public class CassandraMapDatastoreProviderFactory extends AbstractCassandraProviderFactory implements DatastoreProviderFactory, EnvironmentDependentProviderFactory {
+public class CassandraMapDatastoreProviderFactory extends AbstractCassandraProviderFactory implements DatastoreProviderFactory, EnvironmentDependentProviderFactory, InvalidationHandler {
     private static final String PROVIDER_ID = "cassandra-map";
 
     @Override
@@ -57,4 +60,18 @@ public class CassandraMapDatastoreProviderFactory extends AbstractCassandraProvi
         return true;
     }
 
+    @Override
+    public void invalidate(KeycloakSession session, InvalidableObjectType type, Object... params) {
+        if (type == REALM_BEFORE_REMOVE) {
+            create(session).users().preRemove((RealmModel) params[0]);
+        } else if (type == ROLE_BEFORE_REMOVE) {
+            create(session).users().preRemove((RealmModel) params[0], (RoleModel) params[1]);
+        } else if (type == CLIENT_SCOPE_BEFORE_REMOVE) {
+            create(session).users().preRemove((ClientScopeModel) params[1]);
+        } else if (type == CLIENT_BEFORE_REMOVE) {
+            create(session).users().preRemove((RealmModel) params[0], (ClientModel) params[1]);
+        } else if (type == GROUP_BEFORE_REMOVE) {
+            create(session).users().preRemove((RealmModel) params[0], (GroupModel) params[1]);
+        }
+    }
 }
